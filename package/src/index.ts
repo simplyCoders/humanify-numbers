b221aeeda932e7ccbb79981b80c824fefce86800
ezborgy <ezborgy>
2021-02-18 22:52:12 -0500

    Add support for percent and fix bugs.

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,12 +1,19 @@
 // //////////////////////////////////////////////////////////////
 // humanify-numbers
-// Convert numbers to human readbale and short strings.
+// Convert numbers to human readbale / short strings.
 // Developed by simplyCoders, 2021.
+
+const humanifyPercent = (value: number, precision: number) => {
+
+    // convert number to string
+    return ((value*100).toFixed(precision)).toString() + '%'
+
+}
 
 export class Numbers {
   static Humanify(value: number, options?: any) {
-    const numberSuffix = ['', 'K', 'M', 'B', 'T', 'Q']
-    const byteSuffix = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+    const numberSuffix = ['' , 'K' , 'M' , 'B' , 'T' , 'Q']
+    const byteSuffix   = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
     // validate value
     if (typeof value !== 'number') {
@@ -18,6 +25,7 @@ export class Numbers {
 
     const optsByte = (typeof opts.format !== 'string') ? 'number' : opts.format.toLowerCase()
     const isByteFormat = (optsByte === 'byte')
+    const isPercentFormat = (optsByte === 'percent')
 
     const optsPrecision = (typeof opts.precision !== 'number' || !Number.isInteger(opts.precision)) ? 1 : opts.precision
     const precision = (optsPrecision < 0 || optsPrecision > 3) ? 1 : optsPrecision
@@ -31,20 +39,21 @@ export class Numbers {
       return 'NaN'
     }
 
-    if (value === 0) {
-      const zSuffix = (isByteFormat) ? byteSuffix[0] : ''
-      return `0${zSuffix}`
+    // handle percent
+    if (isPercentFormat) {
+      return humanifyPercent(value, precision)
     }
 
     // analyze the number
     const avalue = Math.abs(value)
-    const numberType = Math.floor(Math.log(avalue) / Math.log(10) / 3)
-    const suffix = isByteFormat ? byteSuffix[numberType] : numberSuffix[numberType]
+    let numberCategory = Math.floor(Math.log(avalue+0.0001) / Math.log(10) / 3) // '', 'k', 'm', ..
+    numberCategory = (numberCategory<0) ? 0 : numberCategory
+    const suffix = isByteFormat ? byteSuffix[numberCategory] : numberSuffix[numberCategory]
 
-    const digitsToRemove = numberType * 3 - precision
-    const digitsToKeep = Math.floor(value / (10 ** digitsToRemove)) / (10 ** precision)
+    const digitsToRemove = numberCategory * 3 - precision
+    const digitsToKeep = (Math.floor(value / (10 ** digitsToRemove)) / (10 ** precision)).toFixed(precision)
 
     // convert number to string
-    return (digitsToKeep).toString() + suffix
+    return digitsToKeep + suffix
   }
 }
